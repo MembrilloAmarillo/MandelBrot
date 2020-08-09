6a2c08f6dccba8f47ae13fc0932627022b413ee3
SadenPain <saschapaezsk8@gmail.com>
2020-08-09 22:00:13 +0200

    20200908 - Mas mierda

--- src/linear.cpp
+++ src/linear.cpp
@@ -9,7 +9,7 @@ class mandel_properties {
 	public:
 		long double z_param (long double, long double, long double);
 		long double x_param (long double, long double, long double);
-		long double c_param (long double, long double, long double);
+		long double c_param (long double);
 	/* No las hago privadas porque no puedo acceder desde la herencia a las variables */
 		long double z;
 		long double x;
@@ -20,28 +20,41 @@ class mandel_properties {
 // Herencia para x_sub n+1
 class mandel_next:public mandel_properties {
 	public:
-	long double next_x (long double, long double);
-	
+		long double next_x (long double, long double);
+		/* Debemos que calcular la derivada de next_y para encontrar valores criticos */
+		long double next_y (long double);
 	private:
-	long double x_plus1;
+		long double x_plus1;
 };
 
 long double mandel_properties::z_param(long double z, long double r, long double x) {
 	long double theta = 0;
+	/* No creo que haga falta esto, pero por si acaso :) */
 	z = this->z;
 	r = this->r;
 	x = this->x;
+	/* ################################################ */
 
 	theta = r * (1/2 - x);
 	return theta;
 }
 
+/* Podemos meter algun valor loco de r y al carajo */
+long double mandel_properties::c_param(long double r) {
+	r = this->r;
+
+	/* r = 4 es un valor seguro para calcular */
+	this->c = -(r/2) * ((r-2)/2);
+
+	return this->c;
+}
+
 long double mandel_next::next_x(long double x, long double r) {
 	
-	mandel_properties *op;
-
-	x = op->z_param(this->z, this->r, this->x);
+	/* Lo mismo de antes, por si acaso xD */
+	x = this->x;
 	r = this->r;
+	/* ################################## */
 	this->x_plus1 = 0;
 
 	x_plus1 = r * x * (1 - x);
